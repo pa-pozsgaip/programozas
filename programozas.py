39a19ea91f96fa0bdf5d1bec1db2e1256058ae59
pa-pozsgaip <112852395+pa-pozsgaip@users.noreply.github.com>
2022-10-03 09:34:10 +0200

    Update programozas.py

--- programozas.py
+++ programozas.py
@@ -1,1 +1,10 @@
-
+#1. feladat 9dk
+IDEI_ÉV = 2022
+print(type(IDEI_ÉV))
+print('Az IDEI_ÉV változó értéke: ', IDEI_ÉV, sep='--->')
+felhasználó_kora = input('Hány éves vagy?')
+print(type(felhasználó_kora))
+print('Te most', felhasználó_kora,'éves vagy.')
+felhasználó_kora = int(felhasználó_kora)
+születési_év = IDEI_ÉV - felhasználó_kora
+print('Ekkor születtél: ', születési_év, '.', sep='')
